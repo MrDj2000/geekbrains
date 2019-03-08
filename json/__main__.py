4fd32e5decff3b884d137138c4e6d99f59bff6c1
MrDj2000 <kalinchuk_pvl89@mail.ru>
2019-03-08 23:42:27 +0300

    1.1 CSV JSON YAML

--- json/__main__.py
+++ json/__main__.py
@@ -1,7 +1,29 @@
 import json
 
-with open('json/data.json','w') as file:
+data = [
+    {
+        'name': 'apple',
+        'desc': 'some apple',
+        'cost': 235
+    },
+    {
+        'name': 'apple',
+        'desc': 'some apple',
+        'cost': 236
+    },
+    {
+        'name': 'apple',
+        'desc': 'some apple',
+        'cost': 237
+    }
+]
+
+with open('json/data.json', 'w') as file:
     json.dump(data, file, sort_keys=True, indent=2)
 
 with open('json/data.json') as file:
-    
+    print(
+        type(
+            json.load(file)
+        )
+    )
--- yaml/__main__.py
+++ yaml/__main__.py
@@ -1,4 +1,19 @@
 import yaml
 
-with open('yaml/conf.yml','w') as file:
-    yaml.dump(data, file, sort_keys=True, indent=2)
+data = {
+    'entrypoint': 'main.py',
+    'host': 'loaclhost',
+    'port': 8000
+}
+
+with open('yaml/conf.yml', 'w') as file:
+    yaml.dump(data, file)
+
+with open('yaml/conf.yml') as file:
+    print(
+        type(
+            yaml.load(file)
+        )
+    )
+
+ #   yaml.dump(data, file, sort_keys=True, indent=2)
